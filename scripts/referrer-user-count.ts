e2c1a9d9c2f15626e805cecc8b90b6ed1a2a2a6e
Tom McGuire <mcgtom10@gmail.com>
2025-01-23 23:09:01 -0800

    feat: support n referrers per file

--- scripts/referrer-user-count.ts
+++ scripts/referrer-user-count.ts
@@ -12,27 +12,24 @@ async function getArgs() {
       demandOption: true,
       choices: protocols,
     })
-    // This is coerced as a string array to prevent 0x... from being interpreted as a number
-    .option('referrerAddresses', {
-      description: 'referrer address(es)',
+    .option('output-file', {
+      alias: 'o',
+      description: 'output file path to write JSON results',
       type: 'string',
       demandOption: true,
+    })
+    // This is coerced as a string array to prevent 0x... from being interpreted as a number
+    .option('referrer-addresses', {
+      description: 'referrer address(es)',
+      type: 'string',
+      demandOption: false,
       coerce: (arg) => new Set(arg.split(',')),
     })
-    .option('networkIds', {
+    .option('network-ids', {
       description: 'Comma-separated list of network IDs',
       type: 'string',
       demandOption: false,
-      coerce: (arg: string) => {
-        const networkIds = arg.split(',').map((id) => id.trim())
-        const invalidIds = networkIds.filter(
-          (id) => !Object.values(NetworkId).includes(id as NetworkId),
-        )
-        if (invalidIds.length > 0) {
-          throw new Error(`Invalid network ID(s): ${invalidIds.join(', ')}`)
-        }
-        return networkIds
-      },
+      coerce: (arg: string) => new Set(arg.split(',')),
     }).argv
 
   return {
@@ -40,14 +37,29 @@ async function getArgs() {
     protocolFilter: protocolFilters[argv['protocol'] as Protocol],
     networkIds: (argv['networkIds'] as NetworkId) ?? supportedNetworkIds,
     referrerAddresses: argv['referrerAddresses'] as string,
+    output: argv['output-file'],
   }
+}
+
+function writeResults(
+  outputPath: string,
+  results: Array<{ referrer: string; referralCount: number }>,
+) {
+  const output = results
+    .map((result) => `${result.referrer},${result.referralCount}`)
+    .join('\n')
+  writeFileSync(outputPath, output, { encoding: 'utf-8' })
 }
 
 async function main() {
   const args = await getArgs()
   // Conversions to allow yargs to take in a list of referrer addresses / network IDs
-  const referrerArray = [...args.referrerAddresses]
-  const networkIdsArray = [...args.networkIds] as NetworkId[]
+  const referrerArray = args.referrerAddresses
+    ? [...args.referrerAddresses]
+    : undefined
+  const networkIdsArray = [
+    ...(args.networkIds ?? supportedNetworkIds),
+  ] as NetworkId[]
 
   const referralEvents = await fetchReferralEvents(
     networkIdsArray,
@@ -57,15 +69,33 @@ async function main() {
   const uniqueEvents = removeDuplicates(referralEvents)
   const protocolFilteredEvents = await args.protocolFilter(uniqueEvents)
 
-  referrerArray.forEach((referrer) => {
-    const targetReferrerEvents = protocolFilteredEvents.filter(
-      (event) => event.referrerId === referrer,
-    )
-    const output = `${args.protocol},${targetReferrerEvents.length}`
-    writeFileSync(`${referrer}_referral_count.csv`, output, {
-      encoding: 'utf-8',
+  const allResults: Array<{
+    referrer: string
+    referralCount: number
+  }> = []
+
+  // If referrer addresses are provided output all the addresses and their count
+  // Else output all referrer addresses and their count
+  if (referrerArray) {
+    referrerArray.forEach((referrer) => {
+      const targetReferrerEvents = protocolFilteredEvents.filter(
+        (event) => event.referrerId === referrer,
+      )
+      allResults.push({
+        referrer,
+        referralCount: targetReferrerEvents.length,
+      })
     })
-  })
+  } else {
+    protocolFilteredEvents.forEach((event) => {
+      allResults.push({
+        referrer: event.referrerId,
+        referralCount: 1,
+      })
+    })
+  }
+
+  writeResults(args.output, allResults)
 }
 
 main().catch((error) => {
